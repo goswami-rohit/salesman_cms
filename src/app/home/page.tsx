8afed2565477b55ebbedc2ec2eda40c674f8a6f1
goswami-rohit <rohit.second44@gmail.com>
2025-08-07 10:53:50 +0530

    double login fix try3a

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -3,5 +3,5 @@ import { redirect } from 'next/navigation';
 export default function HomePage() {
   // This page simply serves as a redirector. It sends the user to the
   // signed-in home page, which contains the actual dashboard content.
-  redirect('/home/signedInHomePage');
+  redirect('/home/signedInHomePage.tsx');
 }
