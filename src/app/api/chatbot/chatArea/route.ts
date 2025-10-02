e0f5d3d9606fe0d895e3b646897d926f0d845bb8
Rohit Goswami <rohitgoswami@Rohits-MacBook-Air.local>
2025-10-02 20:41:27 +0530

    adapted Socket IO for CemTemChat4

--- src/app/api/chatbot/chatArea/route.ts
+++
@@ -1,8 +0,0 @@
-// app/api/chatbot/chatArea/route.ts
-// directly used Socket IO in app/home/cemtemChat/page.tsx for real time communication with python-ai-agent
-
-import { NextRequest, NextResponse } from 'next/server';
-
-export async function GET(req: NextRequest) {
-        return NextResponse;
-}
